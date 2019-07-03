c1ea1a736c05e36d7093ed5f90ef8818bbae3ca1
jessindo <U533558@bca>
2019-07-03 15:25:34 +0700

    lesson 5 before hw

---
+++ app/components/nav-link-to.js
@@ -0,0 +1,12 @@
+import LinkComponent from '@ember/routing/link-component';
+
+export default LinkComponent.extend({
+  tagName: 'li'
+});
+
+// Original
+// import Component from '@ember/component';
+
+// export default Component.extend({
+    
+// });
